0f14190c83e5837d7c816e0f97bc82bcb0812d60
Eric Oestrich <eric@oestrich.org>
2014-10-10 15:44:12 -0400

    Pull out key maps into methods

--- json_pointer_middleware.rb
+++ json_pointer_middleware.rb
@@ -15,6 +15,7 @@ class JsonPointerMiddleware < Faraday::Middleware
       begin
         env.body = @body.to_json
       rescue JSON::NestingError
+        # there is a circular nest, skip dereferencing
       end
     end
   end
@@ -27,21 +28,33 @@ class JsonPointerMiddleware < Faraday::Middleware
       next unless value =~ /\//
 
       pointer_keys = value.split("/")[1..-1]
-      pointer_keys = pointer_keys.map do |key|
-        key.gsub("~1", "/")
-      end
-      pointer_keys = pointer_keys.map do |key|
-        key.gsub("~0", "~")
-      end
-      pointer_keys = pointer_keys.map do |key|
-        # Convert array indices to Integers
-        Integer(key) rescue key
-      end
+      pointer_keys = escape_slash(pointer_keys)
+      pointer_keys = escape_tilde(pointer_keys)
+      pointer_keys = convert_indices(pointer_keys)
       new_value = pointer_keys.inject(@body) do |body, pointer_key|
         next if body.nil?
         body[pointer_key]
       end
       body[key] = new_value if new_value
+    end
+  end
+
+  def escape_slash(keys)
+    keys.map do |key|
+      key.gsub("~1", "/")
+    end
+  end
+
+  def escape_tilde(keys)
+    keys.map do |key|
+      key.gsub("~0", "~")
+    end
+  end
+
+  # Convert array indices to Integers
+  def convert_indices(keys)
+    keys.map do |key|
+      Integer(key) rescue key
     end
   end
 end
